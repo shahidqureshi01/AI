5add8b4ca8c6765e2e277aa31139c84398cd5822
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-03-26 11:50:15 +0400

    updated web.py file to scrap data and then store it in a dataframe

--- d7_pandas/web.py
+++ d7_pandas/web.py
@@ -1,11 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
-d={}
-page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.777120000000025&lon=-122.41963999999996#.YFtWia9KhPZ")
-page.status_code
-soup= BeautifulSoup(page.content, 'html.parser')
+import re
+import pandas as pd
 
-p= body.find(id= "seven-day-forecast-container").get_text().replace("\n"," ").replace("\n\n", " ").replace("\n\n\n", " ")
-print(p)
-list= p.strip().split(" ")
-print(list)
+res = requests.get('https://forecast.weather.gov/MapClick.php?lat=37.777120000000025&lon=-122.41963999999996#.YFx-AGQzZb8')
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+html = list(soup.children)[2]
+
+body = list(html.children)[3]
+
+tag = body.find('div', {'id' : 'seven-day-forecast-container'})
+
+text = tag.get_text().replace('\n', ' ').strip().strip()
+
+text = re.sub(' +', ' ', text)
+l = text.split(' ')
+
+
+i = 0
+j = 1
+
+l2 = []
+'''
+below code only extract the data from days and ignore the
+night time data, strores it in the l2 list
+'''
+while(i < len(l)): 
+    if(j == 4 and (i + 3) < len(l)):
+        i += 5
+        j = 1
+    else:
+        l2.append(l[i])
+        i += 1
+        j += 1
+
+# remove °F from the list
+
+l2 = [i for i in l2 if i != '°F']
+
+# help function to convert list to a dictionary
+def Convert(lst):
+    res_dct = {lst[i]: [(f'{lst[i + 1]}, {lst[i + 2 ]}')] for i in range(0, len(lst), 3)}
+    return res_dct
+# convert list to a dictionary
+dict = Convert(l2)
+
+print(dict)
+df = pd.DataFrame(data=dict, index=[1, 2, 3, 4, 5])
+
+df
