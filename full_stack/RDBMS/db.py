a4a0d3e5d2ddb6faac3633b409bf7b1c17d302ee
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-07-12 18:46:43 +0400

    update db file

--- full_stack/RDBMS/db.py
+++ full_stack/RDBMS/db.py
@@ -1,14 +1,16 @@
-from dotenv import dotenv_values
+import os 
+from dotenv import dotenv_values, load_dotenv
 from sqlalchemy import create_engine
+load_dotenv()
 
-config = dotenv_values(".env")
-
-HOST= config.get("HOST")
-DB= config.get("DB")
-USER=config.get("USER")
-PASSWORD=config.get("PASSWORD")
-PORT=config.get("PORT")
+HOST= os.getenv("HOST")
+DB= os.getenv("DB")
+USER=os.getenv("DB")
+PASSWORD=os.getenv("PASSWORD")
+PORT=os.getenv("PORT")
 
 def db_engine():
     engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(USER,PASSWORD,HOST,PORT,DB), echo=True,hide_parameters=True)
     return engine
+
+print(db_engine())
