afee818801803f8962b4d0054e8d77b12ead3e03
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-07-13 18:17:14 +0400

    add insert into db

--- full_stack/RDBMS/db.py
+++ full_stack/RDBMS/db.py
@@ -5,12 +5,10 @@ load_dotenv()
 
 HOST= os.getenv("HOST")
 DB= os.getenv("DB")
-USER=os.getenv("DB")
+USER=os.getenv("USER")
 PASSWORD=os.getenv("PASSWORD")
 PORT=os.getenv("PORT")
-
+print(DB)
 def db_engine():
-    engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(USER,PASSWORD,HOST,PORT,DB), echo=True,hide_parameters=True)
+    engine = create_engine("postgresql://{}:{}@{}:{}/{}".format('shahid',PASSWORD,HOST,PORT,DB), echo=True,hide_parameters=True)
     return engine
-
-print(db_engine())
--- full_stack/RDBMS/main.py
+++ full_stack/RDBMS/main.py
@@ -4,5 +4,8 @@ from sqlalchemy.orm import Session
 
 engine = db_engine()
 session = Session(engine, True)
-user = User(name='Ubeyt',last_name='Demir',email='ubeytdemir4se@gmail.com',password="123")
+user = User(name='Ubeyt',last_name='Demir',email='ubeytdemir4se@gmail.com', id=20, password_hash='test')
 session.add(user)
+session.commit()
+
+user2 = session.query(User).get(20)
