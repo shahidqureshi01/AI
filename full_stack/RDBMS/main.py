823f410df48a1d712089950143e6858bd10a3d4e
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-07-13 11:17:05 +0400

    modify session code

--- full_stack/RDBMS/main.py
+++ full_stack/RDBMS/main.py
@@ -1,5 +1,8 @@
+from users import User
 from db import db_engine
 from sqlalchemy.orm import Session
 
 engine = db_engine()
-session = Session(engine, future=True)
+session = Session(engine, True)
+user = User(name='Ubeyt',last_name='Demir',email='ubeytdemir4se@gmail.com',password="123")
+session.add(user)
