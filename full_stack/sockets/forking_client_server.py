dab95ac68a0bea0bcc1b3f426024c8473fc22c33
shahidqureshi01 <shahidsabir21@hotmail.com>
2021-07-16 17:39:40 +0400

    correct indentation

--- full_stack/sockets/forking_client_server.py
+++ full_stack/sockets/forking_client_server.py
@@ -13,28 +13,28 @@ ECHO_MSG = 'Hello echo server!'
  
  
 class ForkedClient(): 
-    """ A client to test forking server"""     
-    def __init__(self, ip, port): 
-        # Create a socket 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Connect to the server 
-        self.sock.connect((ip, port)) 
-     
-    def run(self): 
-        """ Client playing with the server""" 
-        # Send the data to server 
-        current_process_id = os.getpid() 
-        print ('PID %s Sending echo message to the server : "%s"' % (current_process_id, ECHO_MSG)) 
- 
-        sent_data_length = self.sock.send(bytes(ECHO_MSG, 'utf-8')) 
- 
-        print ("Sent: %d characters, so far..." %sent_data_length) 
-         
-        # Display server response 
-        response = self.sock.recv(BUF_SIZE) 
-        print ("PID %s received: %s" % (current_process_id, response[5:])) 
-     
-    def shutdown(self): 
-        """ Cleanup the client socket """ 
-        self.sock.close() 
-       
+	""" A client to test forking server"""     
+	def __init__(self, ip, port): 
+		# Create a socket 
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# Connect to the server 
+		self.sock.connect((ip, port)) 
+		
+	def run(self): 
+		""" Client playing with the server""" 
+		# Send the data to server 
+		current_process_id = os.getpid() 
+		print ('PID %s Sending echo message to the server : "%s"' % (current_process_id, ECHO_MSG)) 
+
+		sent_data_length = self.sock.send(bytes(ECHO_MSG, 'utf-8')) 
+
+		print ("Sent: %d characters, so far..." %sent_data_length) 
+				
+		# Display server response 
+		response = self.sock.recv(BUF_SIZE) 
+		print ("PID %s received: %s" % (current_process_id, response[5:])) 
+		
+	def shutdown(self): 
+		""" Cleanup the client socket """ 
+		self.sock.close() 
+
